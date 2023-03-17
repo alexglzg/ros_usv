452485fd376cc20296b05a094c6a06c08a65f0ce
Ajay Sathya <sajaysathya@gmail.com>
2023-03-17 17:15:42 +0100

    added the MPC ros component

---
+++ src/mpc.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <math.h>
+#include <eigen3/Eigen/Dense>
+
+#include "ros/ros.h"
+#include "geometry_msgs/Pose2D.h"
+#include "geometry_msgs/Vector3.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/UInt8.h"
+#include <ros/console.h>
+#include <ocp/StageOCPApplication.hpp>
+
+using namespace fatrop;
+class MPC
+{
+    public:
+
+        //Sensor feedback
+        float x = 0.0;
+        float y = 0.0;
+        float psi = 0.0;
+        float u = 0.0;
+        float v = 0.0;
+        float r = 0.0;
+
+        float u_d;
+        float r_d;
+        float u_d_dot;
+        float r_d_dot;
+        float starting_flag=0.0;
+
+        float s = 0.0;
+
+        const double x0[7] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+
+        geometry_msgs::Pose2D desired_speeds;
+        geometry_msgs::Pose2D desired_accelerations;
+
+        std::shared_ptr<StageOCPApplication> app;
+
+        MPC()
+        {
+
+            desired_speeds_pub = n.advertise<geometry_msgs::Pose2D>("/desired_speeds", 1);
+            desired_accelerations_pub = n.advertise<geometry_msgs::Pose2D>("/desired_speeds_derivative", 1);
+
+            ins_pose_sub = n.subscribe("/vectornav/ins_2d/NED_pose", 1000, &MPC::insCallback, this);
+            local_vel_sub = n.subscribe("/vectornav/ins_2d/local_vel", 1000, &MPC::velocityCallback, this);
+
+            app = StageOCPApplicationBuilder::FromRockitInterface("/home/ajay/rockit/rockit/external/fatrop/foobar/casadi_codegen.so",
+         "/home/ajay/rockit/rockit/external/fatrop/foobar/casadi_codegen.json");
+            
+            ///  no dynamic memory allocation
+            // app->Optimize();
+            // ///  retrieve solution
+            // app->LastStageOCPSolution().Eval(eval_expression, u0_result);
+            app->SetOption("tol", 1e-3);
+            app->SetOption("mu_init", 1e-3);
+            app->SetOption("bound_push", 1e-7); // all *_bound_push variables
+            app->SetOption("warm_start_mult_bound_push", 1e-7); 
+            app->SetOption("accept_every_trial_step", false);
+            app->SetOption("iterative_refinement", false); // fast_step_computation
+            app->SetOption("warm_start_init_point", true);
+
+
+            
+        }
+
+        void insCallback(const geometry_msgs::Pose2D::ConstPtr& _ins)
+        {
+            x = _ins -> x;
+            y = _ins -> y;
+            psi = _ins -> theta;
+        }
+
+        void velocityCallback(const geometry_msgs::Vector3::ConstPtr& _vel)
+        {
+            u = _vel -> x;
+            v = _vel -> y;
+            r = _vel -> z;
+        }
+
+        void mpc()
+        {   
+            // TODO: figure out why an extra iteration
+            
+
+            auto param = app->GetParameterSetter("X_0");
+            const double x0[7] = {x, y, psi, u, v, r, s};
+            param->SetValue(x0);
+
+            app->Optimize();
+
+            auto eval_expression = app->GetExprEvaluator("Urdot")->at_t0();
+            std::vector<double> u0_result(eval_expression -> Size());
+
+            auto eval_r = app->GetExprEvaluator("r")->at_tk(1);
+            std::vector<double> r_result(eval_r -> Size());
+
+            auto eval_s = app->GetExprEvaluator("s")->at_tk(1);
+            std::vector<double> s_result(eval_s -> Size());
+
+            app->LastStageOCPSolution().Eval(eval_expression, u0_result);
+            app->LastStageOCPSolution().Eval(eval_r, r_result);
+            app->LastStageOCPSolution().Eval(eval_s, s_result);
+            app->SetInitial(app->LastStageOCPSolution());
+
+            desired_speeds.x = 0.3;
+            desired_speeds.y = starting_flag;
+            desired_speeds.theta = r_result[0];
+            s = s_result[0];
+            starting_flag += 1;
+
+            desired_speeds_pub.publish(desired_speeds);
+        }
+
+    private:
+        ros::NodeHandle n;
+
+        ros::Publisher desired_speeds_pub;
+        ros::Publisher desired_accelerations_pub;
+        
+        ros::Subscriber ins_pose_sub;
+        ros::Subscriber local_vel_sub;
+          
+
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mpc");
+    MPC mpc;
+
+    ros::Rate loop_rate(100);
+
+    while (ros::ok())
+    {
+        mpc.mpc();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
